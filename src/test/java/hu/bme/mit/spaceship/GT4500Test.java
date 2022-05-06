1f5a04e4cf9909b1c2e780399947eeb76d1b803b
botondklenk <botondklenk@gmail.com>
2022-05-06 11:16:20 +0200

    unit tests added with mock and jococo

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,32 +9,49 @@ import static org.mockito.Mockito.*;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore mockPrimary; 
+  private TorpedoStore mockSecondary;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    mockPrimary = mock(TorpedoStore.class); 
+    mockSecondary = mock(TorpedoStore.class); 
+    this.ship = new GT4500(
+      mockPrimary,
+      mockSecondary
+    );
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(mockPrimary.isEmpty()).thenReturn(false); 
+    when(mockPrimary.fire(1)).thenReturn(true); 
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+    verify(mockPrimary, times(1)).fire(1); 
+    verify(mockSecondary, times(0)).fire(1); 
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(mockPrimary.isEmpty()).thenReturn(false); 
+    when(mockPrimary.fire(1)).thenReturn(true); 
+    when(mockSecondary.isEmpty()).thenReturn(false); 
+    when(mockSecondary.fire(1)).thenReturn(true); 
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+    verify(mockPrimary, times(1)).fire(1); 
+    verify(mockSecondary, times(1)).fire(1); 
   }
 
 }
